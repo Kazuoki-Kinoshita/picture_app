8970df7fff457b74211a8a71137d253fc25a7330
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-11 21:13:15 +0900

    新規投稿機能作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,16 @@
 class PicturesController < ApplicationController
+  def new
+    @picture = Picture.new
+  end
+  def create
+    Picture.create(picture_params)
+    redirect_to new_picture_path
+  end
+
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content, :image)
+  end
 end

822d73d5d97cbdad3a9863afa5d7c4ae2c685e4b
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-11 21:34:38 +0900

    一覧表示機能作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,23 @@
 class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+  
   def new
     @picture = Picture.new
   end
+  
   def create
     Picture.create(picture_params)
-    redirect_to new_picture_path
+    redirect_to  pictures_path
   end
+
+  
+
+
+
+
+
 
 
   private

d4a7e08e27e32aae89338e8edb8e54140bf49dcf
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-11 23:15:20 +0900

    投稿編集機能を作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -21,13 +21,17 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    
+    @picture = Picture.find(params[:id])
   end
 
-
-
-
-
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
 
   private
 

de283a365989731d17d6568834568389a05e37c6
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-12 20:34:43 +0900

    buildメソッドに変更

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,14 +10,12 @@ class PicturesController < ApplicationController
   end
 
   def confirm
-    @picture = Picture.new(picture_params)
-    @picture.user_id = current_user.id
+    @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
   
   def create
-    @picture = Picture.new(picture_params)
-    @picture.user_id = current_user.id
+    @picture = current_user.pictures.build(picture_params)
     if params[:back]
       render :new
     else 

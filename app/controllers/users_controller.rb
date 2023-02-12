b5cc4b35b318118d0b96f2ee88d599e899a81895
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-12 17:29:18 +0900

    アカウント登録失敗時のエラーメッセージ表示を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-
-  end
+    @user = User.new(user_params)
+    if @user.save
   
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end

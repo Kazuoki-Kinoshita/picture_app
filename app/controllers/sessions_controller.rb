0336976081d8781945e778dc1a4a15092a8bf55f
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-12 18:22:36 +0900

    ログイン失敗時の機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    
+    else
+      flash.now[:denger] = "ログインに失敗しました！"
+      render :new
+    end
   end
 end
